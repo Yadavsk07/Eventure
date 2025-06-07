b02497c53bda11f45ce6427f6f9563db6a2fa861
Yadavsk07 <kumar.sumit07092003@gmail.com>
2025-06-07 20:57:09 +0530

    Handled image uploads

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [react() , tailwindcss() ],
+  server: {
+    port: 5173,
+    open: true
+  }
 })
